8865a362148f164d8fef9f808bda69cd8eda96bb
Raztyk <117339673+Raztyk@users.noreply.github.com>
2025-06-08 23:39:52 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from core.app import App
+
+if __name__ == "__main__":
+    print("Inicializando app...")
+    app = App()
+    print("Ejecutando mainloop...")
+    app.mainloop()
+    print("App finalizada.")

a9dbad2da026c5dc375b3359a30eefe500fe123c
Raztyk <117339673+Raztyk@users.noreply.github.com>
2025-06-09 01:15:36 -0600

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from core.app import App
-
-if __name__ == "__main__":
-    print("Inicializando app...")
-    app = App()
-    print("Ejecutando mainloop...")
-    app.mainloop()
-    print("App finalizada.")
